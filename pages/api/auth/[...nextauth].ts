ff9eeff12bce3743cb3d2924f086cb61c67aa0e1
shayana-patel <shayana_patel@hotmail.com>
2022-02-22 11:03:56 +1300

    add email provider for nextauth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -4,7 +4,9 @@ import NextAuth from 'next-auth';
 
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import GitHubProvider from 'next-auth/providers/github';
+import EmailProvider from "next-auth/providers/email";
 import prisma from '../../../lib/prisma';
+
 
 const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
 export default authHandler;
@@ -15,6 +17,17 @@ const options = {
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD
+        }
+      },
+      from: process.env.EMAIL_FROM
+    })
   ],
   adapter: PrismaAdapter(prisma),
   secret: process.env.SECRET,
